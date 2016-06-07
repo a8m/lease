1a95567f4850c33e2db1ebfd21a6e828473d4885
Ariel Mashraki <ariel@mashraki.co.il>
2016-06-07 19:10:35 +0300

    feat(coordinator): add gracefull stop function

--- coordinator.go
+++ coordinator.go
@@ -10,6 +10,7 @@ type Coordinator struct {
 	rand    *rand.Rand
 	renewer Renewer
 	taker   Taker
+	done    chan struct{}
 }
 
 // NewCoordinator create new Coordinator with the given config.
@@ -17,6 +18,7 @@ func NewCoordinator(config *Config) *Coordinator {
 	manager := &LeaseManager{config}
 	c := &Coordinator{
 		Config: config,
+		done:   make(chan struct{}),
 		renewer: &LeaseHolder{
 			Config:     config,
 			manager:    manager,
@@ -39,7 +41,23 @@ func (c *Coordinator) GetLeases() []*Lease {
 	return c.renewer.GetHeldLeases()
 }
 
+// Stop the coordinator gracefully. wait for background tasks to complete.
+func (c *Coordinator) Stop() {
+	c.Logger.Info("stopping coordinator")
+
+	// notify loop
+	c.done <- struct{}{}
+
+	// wait
+	<-c.done
+
+	c.Logger.Info("stopped coordinator")
+}
+
+// loop run forever and upadte leases periodically.
 func (c *Coordinator) loop() {
+	defer close(c.done)
+
 	for {
 		// Take(or steal leases)
 		if err := c.taker.Take(); err != nil {
@@ -55,13 +73,17 @@ func (c *Coordinator) loop() {
 			c.Logger.Infof("Worker %s finish to renew leases successfully", c.OwnerId)
 		}
 
-		// wait for a while...
-		<-c.ticker()
+		select {
+		// wait for a while and loop again.
+		case <-c.ticker():
+			continue
+		// or someone called stop and we need to exit.
+		case <-c.done:
+			return
+		}
 	}
 }
 
-// ticker return timer chan between 1-2 minutes
-// TODO: ticker should be configurable with lower and higher threshold.
 func (c *Coordinator) ticker() <-chan time.Time {
 	sleepTime := time.Duration(c.rand.Int63n(c.ExpireAfter.Nanoseconds() / 3))
 	c.Logger.Infof("Worker %s sleep for: %s", c.OwnerId, sleepTime)
