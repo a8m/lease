e663b4ad70d2ddf8ec93c5ebc298a019f7a294ab
Ariel Mashraki <ariel@mashraki.co.il>
2016-09-07 19:45:53 +0300

    fix(renewer): update old held leases

--- renewer.go
+++ renewer.go
@@ -57,11 +57,9 @@ func (l *leaseHolder) Renew() error {
 	for _, lease := range leases {
 		if lease.Owner == l.WorkerId {
 			// if we took this lease and it's not holds by this renewer
-			if _, ok := l.heldLeases[lease.Key]; !ok {
-				l.Lock()
-				l.heldLeases[lease.Key] = lease
-				l.Unlock()
-			}
+			l.Lock()
+			l.heldLeases[lease.Key] = lease
+			l.Unlock()
 			if err := l.manager.RenewLease(lease); err != nil {
 				l.Logger.Debugf("Worker %s could not renew lease with key %s", l.WorkerId, lease.Key)
 			}
